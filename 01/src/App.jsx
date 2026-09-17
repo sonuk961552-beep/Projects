b3407bddc3fe5fa42749e9c1d1e2f59373b85e17
Sonu Kumar <sonuk961552@gmail.com>
2026-09-17 22:45:09 +0530

    update task pad

--- 01/src/App.jsx
+++ 01/src/App.jsx
@@ -4,19 +4,28 @@ import "./App.css";
 const App = () => {
   const [time, setTime] = useState("");
   const [details, setDetails] = useState("");
+  const [task, setTask] = useState([]);
 
   function clickBtn(e) {
     e.preventDefault();
-    console.log(time, details);
+    // console.log(time, details);
 
+          // { kuch bhi add karne par 4 div faltu mai ban rha tha}
+        if(!time.trim() || !details.trim()){
+          return;
+        }
+           
 
+    const copyTask = [...task];
+    copyTask.push({time, details});
+    setTask(copyTask);
+    console.log(copyTask);
 
-    setTime("")
-    setDetails("")
+    setTime("");
+    setDetails("");
   }
 
-  
-// i am chnageing something here
+  // i am chnageing something here
   return (
     <div className="start">
       <div className="left">
@@ -38,16 +47,23 @@ const App = () => {
             placeholder="Descremation"
           ></textarea>
 
-          <button  className="input">
-            Submit
-          </button>
+          <button className="input">Submit</button>
         </form>
       </div>
 
       {/* note pad yha show hoga */}
+     
       <div className="hello">
-        <div className="notepad"> notepad</div>
-        <div className="notepad"> notepad</div>
+        
+        {task.map(function (e, idx) {
+          return (
+            
+            <div key={idx} className="notepad">
+              <h2 className="h2">{e.time}</h2>
+              <p className="p">{e.details}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
